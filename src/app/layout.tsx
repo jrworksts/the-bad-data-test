eb7356e718527711c9e62c59dcabf3d01c11c77c
JRW <jrworksts@gmail.com>
2026-03-21 17:12:26 -0500

    Add ID Pixel and Meta Pixel to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Instrument_Sans, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
 import { buildOrganizationSchema, buildWebsiteSchema } from "@/lib/schema";
@@ -52,7 +53,35 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="idpixel-analytics"
+          src="https://cdn.idpixel.app/v1/idp-analytics-69bf16b99f10cf2f551c09d5.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script id="meta-pixel" strategy="beforeInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2235265177216898');
+fbq('track', 'PageView');`}
+        </Script>
+      </head>
       <body className={`${sans.variable} ${display.variable} font-sans`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2235265177216898&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         {schemas.map((schema, index) => (
           <script
